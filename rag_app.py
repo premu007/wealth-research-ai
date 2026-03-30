9a6e7ee84aa2955dd8f14680ed20784de173f217
Prem Kumar Chetwani <71445265+premu007@users.noreply.github.com>
2026-03-30 03:43:39 -0700

    Fix import path for RecursiveCharacterTextSplitter
    Fix

--- rag_app.py
+++ rag_app.py
@@ -3,7 +3,7 @@ import os
 from groq import Groq
 from pypdf import PdfReader
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
